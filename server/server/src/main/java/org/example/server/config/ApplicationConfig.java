786aa6377fd383d8124a2fe8a0847416ad5a1e93
dokma11 <vule.dok@gmail.com>
2024-07-11 20:57:23 +0200

    config: Add configuration files

---
+++ server/server/src/main/java/org/example/server/config/ApplicationConfig.java
@@ -0,0 +1,42 @@
+package org.example.server.config;
+
+import lombok.RequiredArgsConstructor;
+import org.example.server.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final IUserService userService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public Logger logger() {
+        return LoggerFactory.getLogger(SpringBootApplication.class);
+    }
+
+}
---
+++ server/server/src/main/java/org/example/server/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package org.example.server.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        var modelMapper = new ModelMapper();
+
+        return modelMapper;
+    }
+
+}
