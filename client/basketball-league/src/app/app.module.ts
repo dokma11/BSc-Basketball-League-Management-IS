a16007a4680eddd32d6ef1fe49b0c908b75052bc
dokma11 <vule.dok@gmail.com>
2024-07-14 20:37:22 +0200

    feat: Implement roster management page

--- client/basketball-league/src/app/app.module.ts
+++ client/basketball-league/src/app/app.module.ts
@@ -8,10 +8,20 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LayoutModule } from "./feature-modules/layout/layout.module";
 import { HttpClientModule } from '@angular/common/http';
 import { AuthModule } from './infrastructure/auth/auth.module';
+import { RosterComponent } from './feature-modules/roster/roster.component';
+import { LeagueNewsComponent } from './feature-modules/league-news/league-news.component';
+import { TradeManagementComponent } from './feature-modules/trade-management/trade-management.component';
+import { TradeRequestCardComponent } from './feature-modules/trade-request-card/trade-request-card.component';
+import { PlayerCardComponent } from './feature-modules/player-card/player-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RosterComponent,
+    LeagueNewsComponent,
+    TradeManagementComponent,
+    TradeRequestCardComponent,
+    PlayerCardComponent,
   ],
   imports: [
     BrowserModule,
