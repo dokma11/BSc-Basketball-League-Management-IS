491e88bd5b809e25df57e7aef2563efa9261332d
dokma11 <vule.dok@gmail.com>
2024-07-19 18:49:14 +0200

    style: change style of components for list adding

--- client/basketball-league/src/app/feature-modules/roster-management/player-card/player-card.component.ts
+++ client/basketball-league/src/app/feature-modules/roster-management/player-card/player-card.component.ts
@@ -1,13 +1,38 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { faHeart, faHandPaper, faList } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faHandPaper, faList, faBan } from '@fortawesome/free-solid-svg-icons';
 import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 import { User } from 'src/app/infrastructure/auth/model/user.model';
+import { AddPlayerToListPromptComponent } from '../add-player-to-list-prompt/add-player-to-list-prompt.component';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-player-card',
   templateUrl: './player-card.component.html',
-  styleUrls: ['./player-card.component.css']
+  styleUrls: ['./player-card.component.css'],
+  animations: [
+      trigger("fadeIn", [
+        transition(":enter", [
+            style({ opacity: 0, transform: "translateX(-40px)" }),
+            animate(
+                "0.5s ease",
+                style({ opacity: 1, transform: "translateX(0)" }),
+            ),
+        ]),
+      ]),
+      trigger('buttonState', [
+        state('clicked', style({
+          transform: 'scale(0.9)',
+          opacity: 0.5
+        })),
+        transition('* => clicked', [
+          animate('200ms')
+        ]),
+        transition('clicked => idle', [
+          animate('200ms')
+        ])
+      ]),
+  ],
 })
 export class PlayerCardComponent implements OnInit{
   addToWishlistButtonState: string = 'idle';
@@ -46,18 +71,36 @@ export class PlayerCardComponent implements OnInit{
   }
 
   addToWishlistButtonClicked(player: any){
+    // TODO: Implementirati lpogiku za dodavanje odredjenog igraca na listu zelja, vrv treba u samom modalnom da se to uradi
 
+    this.addToWishlistButtonState = 'clicked';
+    setTimeout(() => { this.addToWishlistButtonState = 'idle'; }, 200);
+    this.dialogRef = this.dialog.open(AddPlayerToListPromptComponent, {
+      data: 'wishlist'
+    });
   }
 
   addToUntouchablesListButtonClicked(player: any){
+    // TODO: Implementirati lpogiku za dodavanje odredjenog igraca na listu zelja, vrv treba u samom modalnom da se to uradi
 
+    this.addToUntouchablesListButtonState = 'clicked';
+    setTimeout(() => { this.addToUntouchablesListButtonState = 'idle'; }, 200);
+    this.dialogRef = this.dialog.open(AddPlayerToListPromptComponent, {
+      data: 'untouchable list'
+    });
   }
   
   addToTradeListButtonClicked(player: any){
+    // TODO: Implementirati lpogiku za dodavanje odredjenog igraca na listu zelja, vrv treba u samom modalnom da se to uradi
 
+    this.addToTradeListButtonState = 'clicked';
+    setTimeout(() => { this.addToTradeListButtonState = 'idle'; }, 200);
+    this.dialogRef = this.dialog.open(AddPlayerToListPromptComponent, {
+      data: 'trade list'
+    });
   }
 
   faHeart = faHeart;
-  faHandPaper = faHandPaper;
+  faBan = faBan;
   faList = faList;
 }
--- client/basketball-league/src/app/feature-modules/roster-management/roster/roster.component.ts
+++ client/basketball-league/src/app/feature-modules/roster-management/roster/roster.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate, state } from '@angular/animations';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSelect } from '@angular/material/select';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { map, Observable, of, startWith, Subject, take, takeUntil } from 'rxjs';
 import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 import { User } from 'src/app/infrastructure/auth/model/user.model';
 
 @Component({
   selector: 'app-roster',
   templateUrl: './roster.component.html',
-  styleUrls: ['./roster.component.css']
+  styleUrls: ['./roster.component.css'],
+  animations: [
+      trigger("fadeIn", [
+        transition(":enter", [
+            style({ opacity: 0, transform: "translateX(-40px)" }),
+            animate(
+                "0.5s ease",
+                style({ opacity: 1, transform: "translateX(0)" }),
+            ),
+        ]),
+      ]),
+      trigger('buttonState', [
+        state('clicked', style({
+          transform: 'scale(0.9)',
+          opacity: 0.5
+        })),
+        transition('* => clicked', [
+          animate('200ms')
+        ]),
+        transition('clicked => idle', [
+          animate('200ms')
+        ])
+      ]),
+  ],
 })
 export class RosterComponent implements OnInit{
   user: User | undefined;
@@ -19,6 +45,47 @@ export class RosterComponent implements OnInit{
   requestsReportButtonState: string = "";
 
   private dialogRef: any;
+  public teams: string[] = [
+    'Brooklyn Nets',
+    'Golden State Warriors',
+    'Los Angeles Lakers',
+    'Los Angeles Clippers',
+    'New Orleans Pelicans',
+    'New York Knicks',
+    'Oklahoma City Thunder',
+    'San Antonio Spurs',
+    'Boston Celtics',
+    'Denver Nuggets',
+    'Minnesota Timberwolves',
+    'Cleveland Cavaliers',
+    'Philadelphia 76ers',
+    'Phoenix Suns',
+    'Sacramento Kings',
+    'Indiana Pacers',
+    'Dallas Mavericks',
+    'Miami Heat',
+    'Orlando Magic',
+    'Chicago Bulls',
+    'Atlanta Hawks',
+    'Toronto Raptors',
+    'Charlotte Hornets',
+    'Washington Wizards',
+    'Detroit Pistons',
+    'Utah Jazz',
+    'Houston Rockets',
+    'Memphis Grizzlies',
+    'Portland Trail Blazers',
+    'Milwaukee Bucks'
+  ];
+
+  public teamCtrl: FormControl<string | null> = new FormControl<string | null>('');
+  public teamFilterCtrl: FormControl<string | null> = new FormControl<string | null>('');
+
+  public filteredTeams: Observable<string[]> = of(this.teams);
+
+  @ViewChild('singleSelect', { static: true }) singleSelect: MatSelect | undefined;
+
+  protected _onDestroy = new Subject<void>();
 
   constructor(private authService: AuthService,
               private dialog: MatDialog,
@@ -28,6 +95,37 @@ export class RosterComponent implements OnInit{
 
   ngOnInit(): void {
     this.getRequests();
+
+    this.teamCtrl.setValue('');
+
+    this.filteredTeams = this.teamFilterCtrl.valueChanges.pipe(
+      startWith(''),
+      map(value => this.filterTeams(value))
+    );
+  }
+
+  private filterTeams(value: any): string[] {
+    const filterValue = value?.toLowerCase() || '';
+    return this.teams.filter(team => team.toLowerCase().includes(filterValue));
+
+    // TODO: Azurirati listu rosterom odgovarajuce ekipe
+  }
+
+  ngAfterViewInit() {
+    this.setInitialValue();
+  }
+
+  ngOnDestroy() {
+    this._onDestroy.next();
+    this._onDestroy.complete();
+  }
+
+  protected setInitialValue() {
+    this.filteredTeams
+      .pipe(take(1), takeUntil(this._onDestroy))
+      .subscribe(() => {
+        this.singleSelect!.compareWith = (a: string, b: string) => a.toLowerCase() === b.toLowerCase();
+      });
   }
 
   getRequests() {
@@ -58,32 +156,6 @@ export class RosterComponent implements OnInit{
     this.getRequests();
   }
 
-  addTourRequestButtonClicked() {
-    // this.tourRequestsButtonState = 'clicked';
-    // setTimeout(() => { this.tourRequestsButtonState = 'idle'; }, 200);
-    // this.dialogRef = this.dialog.open(AddTourRequestFormComponent, {
-    // });
-
-    // if (this.dialogRef) {
-    //   this.dialogRef.afterClosed().subscribe((result: any) => {
-    //     this.getRequests();
-    //   });
-    // }
-  }
-
-  openHandledRequestsReportDialogue(): void {
-    // this.handledRequestsReportButtonState = 'clicked';
-    // setTimeout(() => { this.handledRequestsReportButtonState = 'idle'; }, 200);
-    // this.dialogRef = this.dialog.open(PdfHandledRequestsPromptComponent, {
-    // });
-
-    // if (this.dialogRef) {
-    //   this.dialogRef.afterClosed().subscribe((result: any) => {
-    //     this.getRequests();
-    //   });
-    // }
-  }
-
   showNotification(message: string): void {
     this.snackBar.open(message, 'Close', {
       duration: 3000,
@@ -91,6 +163,4 @@ export class RosterComponent implements OnInit{
       verticalPosition: 'bottom',
     });
   }
-
-  faPlus = faPlus;
 }
